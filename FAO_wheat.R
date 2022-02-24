97e40a70effe100b9f8f7c47d6efc76b10c453bc
Xin Zhao <xinzhao@umd.edu>
2022-02-24 18:48:16 -0500

    add FAO 2020 wheat bilateral trade flows

---
+++ FAO_wheat.R
@@ -0,0 +1,67 @@
+
+library(dplyr)
+library(circlize)
+
+# FAOSTAT access date 2/2/2022  
+# unit: tonne
+read.csv("data/FAOSTAT_TM_Wheat_2020.csv") -> Wheat_TM_2020 
+
+
+Wheat_TM_2020 %>% 
+  mutate(value = value / 1000000) %>%  #Mt
+  filter(value >= 0.5) %>% 
+  mutate(area = replace(area, area == "United States of America", "USA")) %>% 
+  mutate(area = replace(area, area == "Democratic Republic of the Congo", "DR Congo")) %>% 
+  mutate(area = gsub("Federation| of Great Britain and Northern Ireland| Province of| (Bolivarian Republic of)| (Islamic Republic of)", "", area)) %>% 
+  mutate(source = replace(source, source == "United States of America", "USA")) %>% 
+  mutate(source = replace(source, source == "Democratic Republic of the Congo", "DR Congo")) %>% 
+  mutate(source = gsub("Federation| of Great Britain and Northern Ireland| Province of| (Bolivarian Republic of)| (Islamic Republic of)", "", source)) %>% 
+  select(REG_ex = source, REG_im = area, flow = value) ->
+dat_circular
+  
+
+
+
+
+
+#save figure to default wd
+png(file = "Wheat_trade_2020.png", width = 7000, height = 7000, res = 600)
+
+#base plot
+chordDiagram(as.data.frame(dat_circular), 
+             transparency = 0.5, 
+             directional = 1,
+             direction.type = c("diffHeight", "arrows"), 
+             diffHeight = -uh(2, "mm")
+             ,link.arr.type = "big.arrow"
+             ,annotationTrack = c("grid")
+             ,preAllocateTracks = list(list(track.height = c(0.3))
+                                       ,list(track.height = c(0.035))
+             ))
+
+#title(main = "test figure")
+
+circos.track(track.index = 3, panel.fun = function(x, y) {
+  circos.axis(h = 1, labels.cex = 0.8)   
+}, bg.border = NA)
+
+circos.track(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  xplot = get.cell.meta.data("xplot")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  
+  #make text label vertical when space is too small; cex to adjust font size
+  
+  if(abs(xplot[2] - xplot[1]) < 20 | abs(xplot[2] - xplot[1]) > 340) {
+    circos.text(mean(xlim), ylim[1], sector.name, facing = "clockwise",
+                niceFacing = TRUE, adj = c(0, 0.5), col = "black", 
+                cex = 1)
+  } else {
+    circos.text(mean(xlim), ylim[1], sector.name, facing = "inside", 
+                niceFacing = TRUE, adj = c(0.5, 0), col= "black", 
+                cex = 1)
+  }  }, bg.border = NA)
+
+dev.off() #dump
+
--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # GCAMCirclize
 This R project generate circlize figures for **GCAM** to depict trade flows
 
-R packages listed in run.R are required. 
+R packages listed in GCAM_example.R are required. 
 Raw data generated from GCAM are stored in the folder Queriedcsv
 Those data are processed to generate trade flows in the format to be used in circular package in R.
 Example for corn trade flows in 2100:
 ![Image](test.png)
+
+2/24/2022
+Adding a bilateral trade of wheat flows (see FAO_wheat.R)
+![Image](Wheat_trade_2020.png)
 
